17f47d48ec7b461260aa60ab661629dbc31fb947
Paulo <paulo.almeida@mblabs.com.br>
2025-01-11 00:57:39 -0300

    feat: add api

--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   Bar,
   BarChart,
@@ -8,154 +9,41 @@ import {
   YAxis,
   LabelList,
 } from "recharts";
+import { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+type DataProps = {
+  id: number;
+  name: string;
+  avatar: string | undefined;
+  hortoCoins: number;
+};
 
 export const Chart = () => {
-  const data = [
-    {
-      name: "Huigna",
-      hortoCoins: 0,
-      avatar: "/people/huigna.jpg",
-    },
-    {
-      name: "Tiago Denig",
-      hortoCoins: 0,
-      avatar: "/people/denig.jpg",
-    },
-    {
-      name: "Isabely",
-      hortoCoins: 0,
-      avatar: "/people/isabely.jpg",
-    },
-    {
-      name: "Jader",
-      hortoCoins: 0,
-      avatar: "/people/jader.jpg",
-    },
-    {
-      name: "Laryssa",
-      hortoCoins: 0,
-      avatar: "/people/laryssa.jpg",
-    },
-    {
-      name: "Amancio",
-      hortoCoins: 0,
-      avatar: "/people/amancio.jpg",
-    },
-    {
-      name: "Stephany",
-      hortoCoins: 0,
-      avatar: "/people/stephany.jpg",
-    },
-    {
-      name: "Dunga",
-      hortoCoins: 0,
-      avatar: "/people/dunga.jpg",
-    },
-    {
-      name: "Sarah",
-      hortoCoins: 0,
-      avatar: "/people/sarah.jpg",
-    },
-    {
-      name: "Lorenzo",
-      hortoCoins: 0,
-      avatar: "/people/lorenzo.jpg",
-    },
-    {
-      name: "Matheus",
-      hortoCoins: 0,
-      avatar: "/people/matheus.jpg",
-    },
-    {
-      name: "Bruna",
-      hortoCoins: 0,
-      avatar: "/people/bruna.jpg",
-    },
+  const [data, setData] = useState<DataProps[]>([]);
 
-    {
-      name: "Marya",
-      hortoCoins: 0,
-      avatar: "/people/marya.jpg",
-    },
-    {
-      name: "Vinicius",
-      hortoCoins: 0,
-      avatar: "/people/vinicius.jpg",
-    },
-    {
-      name: "Vitor",
-      hortoCoins: 0,
-      avatar: "/people/vitor.jpg",
-    },
-    {
-      name: "Vitória",
-      hortoCoins: 0,
-      avatar: "/people/vitoria.jpg",
-    },
-    {
-      name: "Michele",
-      hortoCoins: 0,
-      avatar: "/people/michele.jpg",
-    },
-    {
-      name: "Alexandre",
-      hortoCoins: 0,
-      avatar: "/people/alexandre.jpg",
-    },
-    {
-      name: "Flávia",
-      hortoCoins: 0,
-      avatar: "/people/flavia.jpg",
-    },
-    {
-      name: "Jean",
-      hortoCoins: 0,
-      avatar: "/people/jean.jpg",
-    },
-    {
-      name: "Murilo",
-      hortoCoins: 0,
-      avatar: "/people/murilo.jpg",
-    },
-    {
-      name: "Natan",
-      hortoCoins: 0,
-      avatar: "/people/natan.jpg",
-    },
-    {
-      name: "Renan",
-      hortoCoins: 0,
-      avatar: "/people/renan.jpg",
-    },
-    {
-      name: "Thiago Sena",
-      hortoCoins: 0,
-      avatar: "/people/thiago_sena.jpg",
-    },
-    {
-      name: "Ana Luiza",
-      hortoCoins: 0,
-      avatar: "/people/ana_luiza.jpg",
-    },
-    {
-      name: "Julia Medeiros",
-      hortoCoins: 0,
-      avatar: "/people/julia.jpg",
-    },
-    {
-      name: "Arthur",
-      hortoCoins: 0,
-      avatar: undefined,
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://6781e9bdc51d092c3dcda776.mockapi.io/ranking"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar os dados:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const renderCustomizedLabel = (props) => {
-    const imageUrl = data.find((item) => item.name === props.name)?.avatar;
+    const imageUrl = data?.find((item) => item.name === props.name)?.avatar;
     const { x, y, width, height, value } = props;
     const imgSize = 30;
 
     const centerY = y + height / 2 - imgSize / 2;
-
     const positionX = value === 0 ? 110 : x + width + 5;
 
     if (!imageUrl) {
@@ -179,31 +67,45 @@ export const Chart = () => {
       </foreignObject>
     );
   };
+
   return (
     <>
-      <ResponsiveContainer width='100%' height='100%'>
-        <BarChart
-          layout='vertical'
-          data={[...data].sort((a, b) => b.hortoCoins - a.hortoCoins)}>
-          <XAxis
-            type='number'
-            domain={[0, (dataMax: number) => dataMax * 1.2]}
-            stroke='#ffffff'
-          />
-          <YAxis
-            dataKey='name'
-            type='category'
-            stroke='#ffffff' // Cor branca para as linhas do eixo Y
-            tick={{ fill: "#ffffff" }} // Cor branca para os nomes no eixo Y
-            width={90}
-          />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey='hortoCoins' fill='#f0b142' name='HortoCoins'>
-            <LabelList dataKey='hortoCoins' content={renderCustomizedLabel} />
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
+      {data.length > 0 ? (
+        <ResponsiveContainer width='100%' height='100%'>
+          <BarChart
+            layout='vertical'
+            data={[...data].sort((a, b) => b.hortoCoins - a.hortoCoins)}>
+            <XAxis
+              type='number'
+              domain={[0, (dataMax) => dataMax * 1.2]}
+              stroke='#ffffff'
+            />
+            <YAxis
+              dataKey='name'
+              type='category'
+              stroke='#ffffff'
+              tick={{ fill: "#ffffff" }}
+              width={90}
+            />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey='hortoCoins' fill='#f0b142' name='HortoCoins'>
+              <LabelList dataKey='hortoCoins' content={renderCustomizedLabel} />
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <CircularProgress sx={{ color: "#f0b142" }} size={80} />
+        </Box>
+      )}
     </>
   );
 };
